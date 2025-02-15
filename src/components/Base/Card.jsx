a6ab2ad3b96250ad314466b1869f885d920e2692
Utpal <dev.theutpal@gmail.com>
2025-02-15 14:31:06 +0530

    FIX: responsive cards

--- src/components/Base/Card.jsx
+++ src/components/Base/Card.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Card = ({ className = "", bg = "", children }) => {
 	return (
-		<div className={`flex flex-col bg-contain min-h-[400px] sm:min-h-[600px] md:min-h-[400px] lg:min-h-[500px] bg-no-repeat xl:min-h-[650px] bg-center text-white py-5 space-y-5 w-full ${className} ${bg}`}>
+		<div className={`flex flex-col bg-contain min-h-[400px] sm:min-h-[600px] md:min-h-[400px] lg:min-h-[500px] bg-no-repeat xl:min-h-[650px] bg-center text-white py-5 space-y-5 w-full ${className}`}>
+			<img className='w-full h-auto aspect-square' src={bg} alt="" />
 			{children}
 		</div>
 	)
--- src/components/CardsView/Cards.jsx
+++ src/components/CardsView/Cards.jsx
@@ -4,24 +4,24 @@ import OutlineButton from '../Base/Button/OutlineButton'
 
 const Cards = () => {
 	return (
-		<div className='grid grid-cols-1 md:grid-cols-2 gap-y-15 gap-x-5 p-5'>
+		<div className='grid grid-cols-1 md:grid-cols-2 gap-5 p-5'>
 
-			<Card bg="bg-[url(/cards/one.jpg)]" className='flex items-center text-center h-full sm:pt-10 md:pt-16'>
-				<div className='w-10/12 lg:w-1/2 flex flex-col items-center space-y-3 pt-8 md:pt-0'>
+			<Card bg="/cards/one.jpg" className='relative flex items-center text-center'>
+				<div className='absolute top-16 w-9/12 md:w-7/12 lg:w-1/2 flex flex-col items-center space-y-3'>
 					<img src="/cards/head1.png" alt="" className='h-8 md:h-10 w-auto'></img>
 					<h3 className='md:text-lg'>United by rythm. Discover the new Black Unity watch strap and face.</h3>
 					<FilledButton className="font-base px-5" text='Buy' rounded={true} />
 				</div>
 			</Card>
-			<Card bg="bg-[url(/cards/two.png)]" className='flex items-center justify-end text-center h-full sm:pt-10 md:pt-16'>
-				<div className='w-10/12 text-black lg:w-1/2 flex flex-col items-center space-y-3 pb-8 md:pb-0'>
+			<Card bg="/cards/two.png" className='relative flex items-center justify-end text-center'>
+				<div className='absolute bottom-16 w-10/12 text-black lg:w-1/2 flex flex-col items-center space-y-3'>
 					<h3 className='text-2xl md:text-3xl lg:text-5xl font-title'>Valentine's Day</h3>
 					<p className='md:text-lg'>Last chance to find something special for your special someone.</p>
 					<FilledButton className="font-base px-5" text='Buy' rounded={true} />
 				</div>
 			</Card>
-			<Card bg="bg-[url(/cards/three.jpg)]" className='flex items-center text-center h-full sm:pt-10 md:pt-16'>
-				<div className='w-10/12 text-black lg:w-1/2 flex flex-col items-center space-y-3 pt-8 md:pt-0'>
+			<Card bg="/cards/three.jpg" className='relative flex items-center text-center'>
+				<div className='absolute top-16 w-10/12 text-black lg:w-1/2 flex flex-col items-center space-y-3'>
 					<h3 className='text-2xl md:text-3xl lg:text-5xl font-title'>MacBook Air</h3>
 					<p className='md:text-lg'>Supercharged by M3</p>
 					<div className='space-x-3'>
@@ -30,8 +30,8 @@ const Cards = () => {
 					</div>
 				</div>
 			</Card>
-			<Card bg="bg-[url(/cards/four.jpg)]" className='flex items-center text-center h-full sm:pt-10 md:pt-16'>
-				<div className='w-10/12 lg:w-1/2 flex flex-col items-center space-y-3 pt-8 md:pt-0'>
+			<Card bg="/cards/four.jpg" className='relative flex items-center text-center'>
+				<div className='absolute bottom-16 w-10/12 lg:w-1/2 flex flex-col items-center space-y-3'>
 					<h3 className='text-2xl md:text-3xl lg:text-5xl font-title'>iPad Pro</h3>
 					<p className='md:text-lg'>Unbelievably thin. Incredibly powerful.</p>
 					<div className='space-x-3'>
@@ -40,8 +40,8 @@ const Cards = () => {
 					</div>
 				</div>
 			</Card>
-			<Card bg="bg-[url(/cards/five.jpg)]" className="flex items-center text-center h-full justify-end sm:pb-10 md:pb-16">
-				<div className='w-10/12 lg:w-1/2 flex flex-col items-center space-y-3 pb-8 md:pb-0'>
+			<Card bg="/cards/five.jpg" className="relative flex items-center text-center">
+				<div className='absolute bottom-16 w-10/12 lg:w-1/2 flex flex-col items-center space-y-3'>
 					<h3 className='text-2xl md:text-3xl lg:text-5xl font-title'>AirPods 4</h3>
 					<p className='md:text-lg'>Iconic. Now supersonic.<br />Available with Noise Cancellation.</p>
 					<div className='space-x-3'>
@@ -50,8 +50,8 @@ const Cards = () => {
 					</div>
 				</div>
 			</Card>
-			<Card bg="bg-[url(/cards/six.jpg)]" className='flex items-center text-center h-full sm:pt-10 md:pt-16'>
-				<div className='w-10/12 lg:w-1/2 text-black flex flex-col items-center space-y-3 pt-8 md:pt-0'>
+			<Card bg="/cards/six.jpg" className='relative flex items-center text-center'>
+				<div className='absolute top-16 w-10/12 lg:w-1/2 text-black flex flex-col items-center space-y-3'>
 					<img src="/cards/head2.png" alt="" className='h-8 md:h-10 w-auto' />
 					<p className='md:text-lg'>Upgrade and save. It's that easy.</p>
 					<div className='space-x-3'>
